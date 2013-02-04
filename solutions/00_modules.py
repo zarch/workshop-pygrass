473c8c666ace292cc0093cf8b9617e2964a72960
Pietro Zambelli <peter.zamb@gmail.com>
2013-02-04 17:40:56 +0100

    Clean and test all the examples, add some exercise

--- solutions/00_modules.py
+++ solutions/00_modules.py
@@ -2,12 +2,14 @@
 # Exercise 2
 #
 from grass.pygrass.modules import general as g
+import subprocess as sub
 gregion = g.region
 
+
 def get_region():
-    gregion(flags='p', stdout_=PIPE)
+    gregion(flags='p', stdout_=sub.PIPE)
     reg = {}
-    for row in gregion.stdout.splitlines():
+    for row in gregion.outputs.stdout.splitlines():
         key, value = row.split(':')
         reg[key.strip()] = value.strip()
     return reg
---
+++ solutions/01_vector.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from grass.pygrass.vector import VectorTopo
+# import the random function
+import random
+
+# import the Point and Region functions?
+from grass.pygrass.vector.geometry import Point
+from grass.pygrass.gis.region import Region
+
+# instantiate a constant
+NPNTS = 10
+
+
+# define a function to produce random points
+def get_random_points(num):
+    # inside current GRASS' region of course
+    reg = Region()
+    # loop over a series of numbers from 0 up to...
+    for _ in xrange(0, num):
+        # use the function randrange() to get both x and y random numbers
+        x = random.randrange(reg.south, reg.north)
+        y = random.randrange(reg.west, reg.east)
+        # at the end of each loop we need to explicitly catch the pairs of x and y numbers
+        # in order to produce a point
+        yield Point(x, y)
+
+
+def rand_vect_points(name, npoints=10, overwrite=True):
+    new = VectorTopo(name)
+    new.open('w', overwrite=overwrite)
+    for pnt in get_random_points(npoints):
+        new.write(pnt)
+    new.close()
+    return new
